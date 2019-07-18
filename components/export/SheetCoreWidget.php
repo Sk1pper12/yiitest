3c1480405187f049ce2992e5c2a9cc3de3e522f8
sk1pper12 <ski980_t2dsn@ukr.net>
2019-07-18 11:39:29 +0300

    Add components

---
+++ components/export/SheetCoreWidget.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\components;
+
+use yii\base\Widget;
+use yii\helpers\Html;
+
+class SheetCore extends Widget
+{
+    public function init()
+    {
+        parent::init();
+    }
+
+    public function run()
+    {
+        parent::run();
+    }
+}
